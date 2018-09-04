3002bcefd69a1f04e5e54d94f83c3a578da4b4cb
Roman Artemenko <RGArtemenko@gmail.com>
2018-09-04 17:09:42 +0300

    added UserProfile model

--- intership/authentication/models.py
+++ intership/authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=12, blank=False)
