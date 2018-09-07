dc00b0a5a92a84fd13b24b84d557ba4bf482abdb
Roman Artemenko <RGArtemenko@gmail.com>
2018-09-07 10:35:57 +0300

    implemented test for 'sigin'

--- intership/authentication/tests.py
+++ intership/authentication/tests.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your tests here.
 
+
 class SignUpTestCase(TestCase):
 
     def setUp(self):
@@ -22,33 +23,47 @@ class SignUpTestCase(TestCase):
     def test_check_user_created(self):
         user_count_before = User.objects.count()
         res = self.client.post('/api/v1.0/auth/signup/', self.valid_user)
-        print('Res1 : %s' % res)
         user_count_after = User.objects.count()
         self.assertEqual(user_count_before + 1, user_count_after)
 
     def test_check_user_not_created(self):
         user_count_before = User.objects.count()
         res = self.client.post('/api/v1.0/auth/signup/', self.invalid_user)
-        print('Res1 : %s' % res)
         user_count_after = User.objects.count()
         self.assertEqual(user_count_before, user_count_after)
 
 
 class SignInTestCase(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        user = User.objects.create_user(username='test122',
+            email='test1@mail.net',
+            first_name='TestName',
+            last_name='TestLastName',
+            password='password'
+                            )
+        user.save()
+
     def setUp(self):
         base_user = {
-            'username': 'test1',
+            'username': 'test122',
             'password': 'password'
         }
 
         self.valid_user = dict(base_user)
         self.invalid_user = dict(base_user)
-        self.invalid_user.update({'password': 'swor'})
+        self.invalid_user.update({'password': 'swodaadaaqqr'})
 
     def test_get_token_success(self):
-        res = self.client.post('/api/v1.0/auth/signin/', self.valid_user)
+        client_data = self.client
+        res = client_data.post('/api/v1.0/auth/signin/', self.valid_user)
+        self.assertIn('Token', res.content.decode('utf-8'))
+
 
     def test_get_token_fail(self):
-        res = self.client.post('/api/v1.0/auth/signup/', self.invalid_user)
+        client_data = self.client
+        res = client_data.post('/api/v1.0/auth/signin/', self.invalid_user)
+        self.assertIn('Wrong password', res.content.decode('utf-8'))
 
